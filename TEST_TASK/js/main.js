4aed9d1d15585e7ebd26905e655b2ee792f0b855
sayanseliv <sayanseliv@gmail.com>
2020-12-28 19:57:40 +0200

    fix mistakes

--- TEST_TASK/js/main.js
+++ TEST_TASK/js/main.js
@@ -1,89 +1,74 @@
 const inputs = document.querySelectorAll(".qty__item");
-const p = document.querySelectorAll("p");
 const button = document.querySelectorAll(".product-box__btn");
 const XXX = document.querySelectorAll(".red-info");
-const box = document.querySelector(".top-header");
 const strXXX = "XXX";
 
-const select = document.querySelectorAll(".select-control");
-const selectCategory = select[0];
-const selectPrice = select[1];
 const items = document.querySelectorAll(".product-box__item");
 
 const order = document.querySelector(".btn-check");
-
-selectCategory.addEventListener("change", function (event) {
-  items.forEach((element) => {
-    if (Number(this.value) === 0) {
-      selectPrice.value = 0;
-      element.style.display = "flex";
-    } else if (
-      element.classList.contains(this.value) &&
-      element.classList.contains("choose") === true
-    ) {
-      element.classList.add("checked");
-      element.style.display = "flex";
-    } else if (!element.classList.contains(this.value)) {
-      element.classList.remove("checked");
-      element.style.display = "none";
-    }
+//Filtration by category
+document
+  .querySelector(".select-box .select-control")
+  .addEventListener("change", function (event) {
+    items.forEach((element) => {
+      if (this.value === "0") {
+        element.classList.remove("hideCategory");
+      } else {
+        element.classList.add("hideCategory");
+        if (element.classList.contains(`${this.value}`)) {
+          element.classList.remove("hideCategory");
+        }
+      }
+    });
   });
-});
-
-selectPrice.addEventListener("change", function (event) {
-  items.forEach((element) => {
-    if (
-      Number(this.value) === 0 &&
-      element.classList.contains("checked") === false
-    ) {
-      selectCategory.value = 0;
-      element.style.display = "flex";
-    } else if (
-      Number(this.value) >=
-        numFromText(element.children[2].children[0].innerHTML) &&
-      element.classList.contains("checked") === true
-    ) {
-      element.classList.add("choose");
-      element.style.display = "flex";
-    } else if (
-      Number(this.value) <
-      numFromText(element.children[2].children[0].innerHTML)
-    ) {
-      element.style.display = "none";
-      element.classList.remove("choose");
-    }
+//Filtration by price
+document
+  .querySelector(".price-select-box .select-control")
+  .addEventListener("change", function (event) {
+    items.forEach((element) => {
+      if (this.value === "0") {
+        element.classList.remove("hidePrice");
+      } else {
+        element.classList.add("hidePrice");
+        if (
+          Number(this.value) >=
+          numFromText(element.children[2].children[0].innerHTML)
+        ) {
+          element.classList.remove("hidePrice");
+        }
+      }
+    });
   });
-});
 
-function giveID(tag, txt) {
-  let start = 0;
-  tag.forEach((el) => (el.id = `${txt}_${(start += 1)}`));
-}
-
-giveID(p, "p");
+giveID(document.querySelectorAll("p"), "p");
 giveID(inputs, "inputs");
 giveID(button, "p");
 
 button.forEach.call(button, function (el) {
   //вешаем событие
+  var prevArray = new Array(button.length).fill(0);
   el.onclick = function (e) {
+    if (XXX[0].innerText === strXXX) {
+      XXX[0].innerText = 0;
+      XXX[1].innerText = 0;
+    }
+
     let ID = numFromText(el.id);
+    let prev = prevArray[ID];
     let inputNumber = numFromText(
       document.getElementById(`inputs_${ID}`).value
     );
     let price = numFromText(document.getElementById(`p_${ID}`).innerText);
-    if (!inputNumber) {
-      return;
-    } else {
-      if (XXX[1].innerText === strXXX && XXX[0].innerText === strXXX) {
-        XXX[0].innerText = inputNumber;
-        XXX[1].innerText = multiply(price, inputNumber);
-      } else {
-        let numBasket = Number(XXX[0].innerText) + inputNumber;
-        let numPrice = Number(XXX[1].innerText) + multiply(price, inputNumber);
-        XXX[0].innerText = numBasket;
-        XXX[1].innerText = numPrice;
-      }
+
+    if (inputNumber >= 0 && inputNumber !== prev) {
+      let differenceValue = inputNumber - prev;
+      let amountCurrent = Number(XXX[0].innerText);
+      XXX[0].innerText = amountCurrent + differenceValue;
+      prevArray[ID] = inputNumber;
+
+      let priceCurrent = Number(XXX[1].innerText);
+      let resPrice = price * differenceValue;
+      XXX[1].innerText = priceCurrent + resPrice;
     }
   };
 });
@@ -93,24 +78,25 @@ order.addEventListener("click", function (event) {
     alert(`Ничего не выбрано, пожалуйста выберите сначала блюдо`);
   } else {
     order.disabled = true;
-    box.insertAdjacentHTML("beforeBegin", modalStructure());
+    document
+      .querySelector(".top-header")
+      .insertAdjacentHTML("beforeBegin", modalStructure());
     let modal = document.getElementById("modal");
     modal.style = modalStyle();
-    let content = document.querySelector(".modal-content");
-    content.style = contentStyle();
+    document.querySelector(".modal-content").style = contentStyle();
     let close = document.querySelector(".close");
     close.style = closeStyle();
-    let wrapper = document.querySelector(".inputs-wrapper");
-    wrapper.style = wrapperStyle();
+    document.querySelector(".inputs-wrapper").style = wrapperStyle();
     close.addEventListener("click", function (event) {
-      modal.style.display = modal.style.display.replace("flex", "none");
+      document.querySelector(".formWithValidation").remove();
+      order.disabled = false;
     });
     let form = document.querySelector(".formWithValidation");
     let name = form.querySelector(".name_input");
     let email = form.querySelector(".email_input");
     let fields = form.querySelectorAll(".field");
-
     form.addEventListener("submit", function (event) {
+      let newStr = name.value[0].toUpperCase() + name.value.slice(1);
       for (let i = 0; i < fields.length; i++) {
         if (!fields[i]) {
           event.preventDefault();
@@ -118,26 +104,29 @@ order.addEventListener("click", function (event) {
         }
       }
 
-      if (!validateName(name.value)) {
+      if (!validateName(newStr)) {
         event.preventDefault();
-        alert(`Имя ${name.value} введено не корректно`);
+        alert(
+          `Имя ${name.value} введено не корректно. Имя может содержать только буквы и дефис`
+        );
       } else if (!validateEmail(email.value)) {
         event.preventDefault();
         alert(`Email ${email.value} введен не корректно`);
-      } else if (validateEmail(email.value) && validateName(name.value)) {
-        event.stopPropagation();
+      } else if (validateEmail(email.value) && validateName(newStr)) {
         alert(
           `Спасибо за покупку в количестве ${XXX[0].innerText} штук на сумму ${XXX[1].innerText} грн.`
         );
+        event.stopPropagation();
         (XXX[0].innerText = strXXX), (XXX[1].innerText = strXXX);
       }
-      console.log("clicked on validate");
-      console.log("name_input: ", name.value);
-      console.log("email_input: ", email.value);
       order.disabled = false;
     });
   }
 });
+
+function giveID(tag, txt) {
+  tag.forEach((el, index) => (el.id = `${txt}_${index}`));
+}
 
 function numFromText(el) {
   return parseInt(el.match(/\d+/));
@@ -148,7 +137,7 @@ function multiply(a, b) {
 }
 
 function validateName(str) {
-  let nameRegex = /^([А-Я]{1}[а-яё]{1,}|[A-Z]{1}[a-z]{1,})$/; //Имя с Заглавной буквы англ/рус
+  let nameRegex = /^([А-Я]{1}[а-яё-]{1,}|[A-Z]{1}[a-z-]{1,})$/; //Имя с Заглавной буквы англ/рус
   return nameRegex.test(str);
 }
 
@@ -165,7 +154,7 @@ function modalStructure() {
 <div class="inputs-wrapper">
 <label for='name_input'>Имя: </label>
 <input type="text" class="name_input field" name="name" required placeholder="Имя"><Br>
-<label for='email_input'>Имя: </label>
+<label for='email_input'>Email: </label>
 <input type="email" class="email_input field" name="email" required placeholder="example@gmail.com"><Br><Br>
 <input type="submit" class="validateBtn" value='Submit'>
 </div>
@@ -211,6 +200,7 @@ function closeStyle() {
 function wrapperStyle() {
   return `text-align: left;`;
 }
+
 inputs.forEach((element) => {
   //ограничить диапазон инпут
   element.addEventListener("input", function (event) {
